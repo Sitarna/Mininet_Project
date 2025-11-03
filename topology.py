a49d6052bee9385fab8358e13e93cac51ce615a3
Maxim Johansson <SHA256:Rb6OM53GlBQNShKYr/V+D0VNR5wt+y6hCjygOtU1uVA>
2025-11-03 04:18:53 -0800

    added template X and template Y

--- topology.py
+++ topology.py
@@ -5,37 +5,49 @@
 # - sudo -E mn --controller=remote,ip=127.0.0.1,port=6653 --custom topology.py --topo network_from_truck
 #
 #
-
-import os
-os.system('sudo mn -c > /dev/null 2>&1')
-
-
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import RemoteController, OVSSwitch
 from mininet.link import TCLink
+from mininet.cli import CLI
 
 class network_from_truck(Topo):
     def build(self):
+        # Hosts
+        
         uav = self.addHost('uav')
         gcs = self.addHost('gcs')
+        
+        # Switches
         uav_sw = self.addSwitch('uav_sw', dpid='0000000000000001')
         gcs_sw = self.addSwitch('gcs_sw', dpid='0000000000000002')
-        self.addLink(uav, uav_sw)
-        self.addLink(gcs, gcs_sw)
-        self.addLink(
-            uav_sw, gcs_sw,
-            cls=TCLink,
-            bw=4,          # bandwidth is 4 Mbps
-            delay='10ms',   # 10 milliseconds latency
-            loss=1,         # 1% packet loss
-        )
         
+        
+# Template X — Control/C2: small UDP, high priority, low latency/jitter    
+# bandwidth is 128 kbps, 2 milliseconds latency, 0% packet loss
 
-if __name__ == '__main__':
-    net = Mininet(topo=network_from_truck(), controller=RemoteController, switch=OVSSwitch)
+        self.addLink(uav, uav_sw, cls=TCLink, bw=0.128, delay='2ms', loss=0)
+        self.addLink(uav_sw, gcs_sw, cls=TCLink, bw=0.128, delay='2ms', loss=0)
+     
+# Template Y — Video/Telemetry: higher bitrate, guaranteed minimum rate    
+# bandwidth is 4 Mbps, 10 milliseconds latency, 1% packet loss
+
+        self.addLink(gcs, gcs_sw, cls=TCLink, bw=4, delay='10ms', loss=1)
+        self.addLink(uav_sw, gcs_sw, cls=TCLink, bw=4, delay='10ms', loss=1)
+
+
+def main():
+    net = Mininet(topo=network_from_truck(),
+                  controller=RemoteController,
+                  switch=OVSSwitch)
     net.start()
-    #net.pingAll()
+    CLI(net)
+    net.stop()
 
-
+# Make the topo visible to CLI
 topos = { 'network_from_truck': network_from_truck }
+
+
+# ---------- Start ----------
+if __name__ == '__main__':
+    main()

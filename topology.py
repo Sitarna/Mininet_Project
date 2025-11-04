3b8684746bcbe8194725ec15e20e90b8fab65ae1
Maxim Johansson <SHA256:Rb6OM53GlBQNShKYr/V+D0VNR5wt+y6hCjygOtU1uVA>
2025-11-04 05:44:48 -0800

    change switch

--- topology.py
+++ topology.py
@@ -2,7 +2,8 @@
 #
 # UAV host <=> uav_sw <=> gcs_sw <=> GCS host
 # Run topology
-# - Run topology and ping all: sudo python3 topology.py
+# Clean previous Mininet sessions: sudo mn -c
+# Run topology and ping all: sudo -E python3 topology.py
 # - sudo -E mn --custom topology.py --topo network_from_truck --controller=remote,ip=127.0.0.1,port=6653  --switch=ovs,protocols=OpenFlow13 
 #
 #
@@ -15,14 +16,13 @@ from mininet.cli import CLI
 class network_from_truck(Topo):
     def build(self):
         # Hosts
+        gcs = self.addHost('gcs', ip='10.0.0.1/24')
+        uav = self.addHost('uav', ip='10.0.0.2/24')
         
-        uav = self.addHost('uav', ip='10.0.0.1/24')
-        gcs = self.addHost('gcs', ip='10.0.0.2/24')
         
         # Switches
-        uav_sw = self.addSwitch('uav_sw', dpid='0000000000000001')
-        gcs_sw = self.addSwitch('gcs_sw', dpid='0000000000000002')
-        
+        gcs_sw = self.addSwitch('gcs_sw', dpid='0000000000000001')
+        uav_sw = self.addSwitch('uav_sw', dpid='0000000000000002')
         
 # Template X — Control/C2: small UDP, high priority, low latency/jitter    
 # bandwidth is 128 kbps, 2 milliseconds latency, 0% packet loss
@@ -34,7 +34,7 @@ class network_from_truck(Topo):
 # bandwidth is 4 Mbps, 10 milliseconds latency, 1% packet loss
 
         self.addLink(gcs, gcs_sw, cls=TCLink, bw=4, delay='10ms', loss=1)
-        self.addLink(uav_sw, gcs_sw, cls=TCLink, bw=4, delay='10ms', loss=1)
+       # self.addLink(gcs_sw, uav_sw, cls=TCLink, bw=4, delay='10ms', loss=1)
 
 
 def main():
@@ -53,7 +53,7 @@ def main():
     print("UAV IP:", uav.IP())
     print("GCS IP:", gcs.IP())
 
-    # Optional: test connectivity
+    # test connectivity
     net.ping([uav, gcs])
 
     CLI(net)
